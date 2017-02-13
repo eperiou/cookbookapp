6ac2476eccf8b2c461437b2cdaa9a5e838421d93
Eric Periou <eperiou@yahoo.com>
2017-02-13 13:16:09 -0600

    Add sign in, sign up, post routes, added mongodb,  body-parser. Started boilerplate for hookup to database.

--- code/server.js
+++ code/server.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser= require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}))
+const MongoClient = require('mongodb').MongoClient
 
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', function (req, res){
-  res.sendFile(__dirname + '/signin.html')
-  // res.send('we got connnections');
+MongoClient.connect('link-to-mongodb', (err, database) => {
+  // ... start the server
 })
 
 
-app.listen(3000,function (){
-  console.log('server connected');
-})
+
+
+
+
+// make routes, post recipe, get recipes, search database
+app.get('/recipes',function(req,res){
+  //render user recipes
+});
+
+
+app.post('/signup',function(req,res){
+  console.log(req.body);
+});
+app.post('/signin',function(req,res){
+  console.log(req.body);
+});
+
+
+
+
+///server startup stuff;
+
+app.get('/', function (req, res){res.sendFile(__dirname + '/signin.html')});
+app.listen(3000,function (){console.log('server connected');});

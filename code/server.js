3ba490ac704f4b0ad4288b69239128c488c21405
Eric Periou <eperiou@yahoo.com>
2017-02-13 13:32:45 -0600

    Adds connection to DB

--- code/server.js
+++ code/server.js
@@ -4,9 +4,15 @@ const mongoose = require('mongoose');
 const bodyParser= require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}))
 const MongoClient = require('mongodb').MongoClient
-
-MongoClient.connect('link-to-mongodb', (err, database) => {
-  // ... start the server
+//mongo db for sandbox environment
+var db;
+MongoClient.connect('mongodb://eperiou:Tsukiyomi55@ds149329.mlab.com:49329/ezpz', (err, database) => {
+  console.log('Mongodb connected');
+  app.listen(3000,function () {console.log('server connected');});
+  if(err) {console.log(err);}
+  else {
+    db = database;
+  }
 })
 
 
@@ -15,15 +21,22 @@ MongoClient.connect('link-to-mongodb', (err, database) => {
 
 
 // make routes, post recipe, get recipes, search database
-app.get('/recipes',function(req,res){
+app.get('/recipes', (req,res)=>{
   //render user recipes
 });
 
 
-app.post('/signup',function(req,res){
-  console.log(req.body);
+app.post('/signup',(req,res)=>{
+  db.collection('users').save(req.body,(err,result)=>{
+    if (err) { console.log(err);
+    } else {
+      console.log('user added to database')
+      res.redirect('/')
+    }
+  })
+
 });
-app.post('/signin',function(req,res){
+app.post('/signin', (req,res)=>{
   console.log(req.body);
 });
 
@@ -33,4 +46,3 @@ app.post('/signin',function(req,res){
 ///server startup stuff;
 
 app.get('/', function (req, res){res.sendFile(__dirname + '/signin.html')});
-app.listen(3000,function (){console.log('server connected');});

52549634f4736811af5989fa5bedefb8cd226e4a
Eric Periou <eperiou@yahoo.com>
2017-02-13 15:21:33 -0600

    Add base page for angular

--- code/server.js
+++ code/server.js
@@ -1,48 +1,48 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-const bodyParser= require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}))
-const MongoClient = require('mongodb').MongoClient
-//mongo db for sandbox environment
-var db;
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+let db;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+  //  mongo db for sandbox environment
+
 MongoClient.connect('mongodb://eperiou:Tsukiyomi55@ds149329.mlab.com:49329/ezpz', (err, database) => {
-  console.log('Mongodb connected');
-  app.listen(3000,function () {console.log('server connected');});
-  if(err) {console.log(err);}
-  else {
-    db = database;
-  }
-})
-
-
-
-
-
+    console.log('Mongodb connected');
+    app.listen(3000, function () { console.log('server connected'); });
+    if (err) {
+        console.log(err);
+    } else {
+        db = database;
+    }
+});
 
 // make routes, post recipe, get recipes, search database
-app.get('/recipes', (req,res)=>{
+app.get('/recipes', (req, res) => {
   //render user recipes
 });
 
 
-app.post('/signup',(req,res)=>{
-  db.collection('users').save(req.body,(err,result)=>{
-    if (err) { console.log(err);
-    } else {
-      console.log('user added to database')
-      res.redirect('/')
-    }
-  })
-
+app.post('/signup', (req, res) => {
+    console.log('signup');
+    db.collection('users').save(req.body, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('user added to database');
+            res.redirect('/');
+        }
+    });
 });
-app.post('/signin', (req,res)=>{
-  console.log(req.body);
+app.post('/signin', (req, res) => {
+    console.log('signin');
+  // console.log(req.body);
 });
 
-
-
-
-///server startup stuff;
-
-app.get('/', function (req, res){res.sendFile(__dirname + '/signin.html')});
+//  server startup stuff;
+app.get('/signin',function(req,res) { res.sendFile( __dirname + 'signin.html');});
+app.get('/signup',function(req,res) { res.sendFile( __dirname + '/signup.html');});
+app.get('/', function(req, res) { res.sendFile( __dirname + '/signin.html'); });

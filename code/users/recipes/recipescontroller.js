b1e53b62cd7460e9f918115dc184baacc0713ece
Eric Periou <eperiou@yahoo.com>
2017-02-14 16:57:09 -0600

    Adds styling, self-updating ingredient list

--- code/users/recipes/recipescontroller.js
+++ code/users/recipes/recipescontroller.js
@@ -2,9 +2,9 @@ angular.module('myApp.recipes', [])
 .controller('RecipesController', function ($scope,Recipes) {
     console.log('myapp module');
     $scope.data = {};
-
     $scope.data.recipes = [];
-
+    $scope.ingredientlist ={};
+    $scope.ingredientlist.ingredients = [];
 
 //need a function to get all recipes
     $scope.getRecipes =function () {
@@ -18,7 +18,10 @@ angular.module('myApp.recipes', [])
         });
     };
 
-
+    $scope.addingredient = function (ingredient) {
+        console.log('click',ingredient);
+        $scope.ingredientlist.ingredients.push(ingredient);
+    };
 //need a function to add one
     $scope.addOne = function () {
         console.log($scope.user,'scope recipe');
@@ -36,8 +39,7 @@ angular.module('myApp.recipes', [])
         console.log('search',$scope.query);
         Recipes.searchRecipe($scope.query)
             .then(function(recipe) {
-                
-                console.log(recipe);
+                console.log(recipe, 'in controller');
             })
             .catch(function(err) {
                 console.log(err);

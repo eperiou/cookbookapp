8eaab13cfd706e8ea6639642e19906d72b3b0cf2
Eric Periou <eperiou@yahoo.com>
2019-08-25 17:50:21 -0500

    (CSS) adds webpack plugin to handle CSS bundling and fixes broken signup/signin switching on the app router

--- code/users/main.js
+++ code/users/main.js
@@ -1,14 +1,15 @@
-var styles = require('../styles/style.css');
-var  angular = require("../bower_components/angular/index");
-var  angularRoute = require("../bower_components/angular-route/index");
+var  angular = require('../bower_components/angular/index');
+var  angularRoute = require('../bower_components/angular-route/index');
 var jquery = require('../bower_components/jquery/dist/jquery');
-var  bootstrap = require("../bower_components/bootstrap4/dist/js/bootstrap.bundle.js");
-var bootstrapStylev = require("../bower_components/bootstrap4/dist/css/bootstrap.css");
+var  bootstrap = require('../bower_components/bootstrap4/dist/js/bootstrap.bundle.js');
+var bootstrapStylev = require('../bower_components/bootstrap4/dist/css/bootstrap.css');
 var recipes = require('./recipes/recipescontroller');
 var requests = require('./requests/requests');
 var requests = require('./signup/signupcontroller');
-
-
+var signupTemplate = require('./signup/signup.html');
+var signinTemplate = require('./signup/signin.html');
+var recipeTemplate = require('./recipes/recipes.html');
+var style = require('./styles/style.css');
 
 angular.module('myApp', ['ngRoute',
     'myApp.recipes',
@@ -19,15 +20,15 @@ angular.module('myApp', ['ngRoute',
 .config(['$routeProvider', function($routeProvider,$httpProvider) {
     $routeProvider.
     when('/', {
-        templateUrl: 'signup/signin.html',
-        controller: 'SignupController'
+        template: signinTemplate,
+        controller: 'SigninController'
     })
     .when('/signup', {
-        templateUrl: 'signup/signup.html',
+        template: signupTemplate,
         controller: 'SignupController'
     })
     .when('/recipes',{
-        templateUrl: 'recipes/recipes.html',
+        template: recipeTemplate,
         controller: 'RecipesController'
     })
     .otherwise({
--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,85 +1,102 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  entry: {
-    app: path.join(__dirname, './users/main.js'),
-    //server: path.join(__dirname, './server/server.js')
-  },
-  output: {
-    filename: 'app-bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      /*{
-        test: require.resolve('jquery'),
-        use: [{
-          loader: 'expose-loader',
-          options: 'jQuery'
-        },{
-          loader: 'expose-loader',
-          options: '$'
-        }]
-      },*/
-      
-      { 
-        test: /\.js$/, 
-        use: 'babel-loader', 
-        exclude: /node_modules/ 
-      },
-      { 
-        test: /\.css$/,
-        loaders:[
-          'style-loader',
-          'css-loader' 
-        ],
-      },
-      { 
-        test: /\.sass$/, 
-        loaders: [
-          'style-loader', 
-          'css-loader'
-        ] 
-      },
-      { 
-        test: /\.html$/, 
-        use: 'raw-loader' 
-      },
-      // inline base64 URLs for <=8k images, direct URLs for the rest
-      { 
-        test: /\.(png|jpg)$/, 
-        use: 'url-loader?limit=8192' 
-      },
-      // helps to load bootstrap's css.
-      { 
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        use: 'url?limit=10000&minetype=application/font-woff' 
-      },
-      { 
-        test: /\.woff2$/,
-        use: 'url?limit=10000&minetype=application/font-woff' 
-      },
-      { 
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: 'url?limit=10000&minetype=application/octet-stream' 
-      },
-      { 
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: 'file' 
-      },
-      { 
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: 'url?limit=10000&minetype=image/svg+xml' 
-      }
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devServer: {
-    publicPath: '/',
-    contentBase: path.join(__dirname, '/public'),
-    compress: true
-  },
-  devtool: 'eval'
-}
+    entry: {
+        app: path.join(__dirname, './users/main.js'),
+        //server: path.join(__dirname, './server/server.js')
+    },
+    output: {
+        filename: 'app-bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            /*{
+                test: require.resolve('jquery'),
+                use: [{
+                loader: 'expose-loader',
+                options: 'jQuery'
+                },{
+                loader: 'expose-loader',
+                options: '$'
+                }]
+            },*/
+            
+            { 
+                test: /\.js$/, 
+                use: 'babel-loader', 
+                exclude: /node_modules/ 
+            },
+            { 
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: '../',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                ],
+            },
+            { 
+                test: /\.sass$/, 
+                loaders: [
+                    'style-loader', 
+                    'css-loader'
+                ] 
+            },
+            { 
+                test: /\.html$/, 
+                use: 'html-loader' 
+            },
+            // inline base64 URLs for <=8k images, direct URLs for the rest
+            { 
+                test: /\.(png|jpg)$/, 
+                use: 'url-loader?limit=8192' 
+            },
+            // helps to load bootstrap's css.
+            { 
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'url?limit=10000&minetype=application/font-woff' 
+            },
+            { 
+                test: /\.woff2$/,
+                use: 'url?limit=10000&minetype=application/font-woff' 
+            },
+            { 
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'url?limit=10000&minetype=application/octet-stream' 
+            },
+            { 
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'file' 
+            },
+            { 
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'url?limit=10000&minetype=image/svg+xml' 
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
+    devServer: {
+        publicPath: '/',
+        contentBase: path.join(__dirname, '/public'),
+        compress: true
+    },
+    devtool: 'eval'
+};

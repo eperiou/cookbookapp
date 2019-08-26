b35852e5844a991c7a4868838022ba6f92703361
Eric Periou <eperiou@yahoo.com>
2019-08-25 21:41:54 -0500

    (feature) removes some bower packages for npm packages and adds auth0 packages

--- code/users/main.js
+++ code/users/main.js
@@ -1,7 +1,7 @@
-var  angular = require('../bower_components/angular/index');
-var  angularRoute = require('../bower_components/angular-route/index');
-var jquery = require('../bower_components/jquery/dist/jquery');
-var  bootstrap = require('../bower_components/bootstrap4/dist/js/bootstrap.bundle.js');
+var angular = require('angular');
+var angularRoute = require('angular-route');
+var jquery = require('jquery');
+var bootstrap = require('../bower_components/bootstrap4/dist/js/bootstrap.bundle.js');
 var bootstrapStylev = require('../bower_components/bootstrap4/dist/css/bootstrap.css');
 var recipes = require('./recipes/recipescontroller');
 var requests = require('./requests/requests');
@@ -9,12 +9,15 @@ var requests = require('./signup/signupcontroller');
 var signupTemplate = require('./signup/signup.html');
 var signinTemplate = require('./signup/signin.html');
 var recipeTemplate = require('./recipes/recipes.html');
+var auth0 = require('../bower_components/auth0.js/build/auth0');
+var angularauth = require('../bower_components/angular-auth0/src/index');
 var style = require('./styles/style.css');
 
 angular.module('myApp', ['ngRoute',
     'myApp.recipes',
     'myApp.requests',
-    'myApp.signup'
+    'myApp.signup',
+    'auth0.auth0'
 
 ])
 .config(['$routeProvider', function($routeProvider,$httpProvider) {

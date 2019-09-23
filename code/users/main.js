be78027ae1eb3b40f644150948f21d2b90226c19
Eric Periou <eperiou@yahoo.com>
2019-09-23 10:54:40 -0500

    Revert "(feature) removes some bower packages for npm packages and adds auth0 packages"
    This reverts commit b35852e5844a991c7a4868838022ba6f92703361.

--- code/users/main.js
+++ code/users/main.js
@@ -1,7 +1,7 @@
-var angular = require('angular');
-var angularRoute = require('angular-route');
-var jquery = require('jquery');
-var bootstrap = require('../bower_components/bootstrap4/dist/js/bootstrap.bundle.js');
+var  angular = require('../bower_components/angular/index');
+var  angularRoute = require('../bower_components/angular-route/index');
+var jquery = require('../bower_components/jquery/dist/jquery');
+var  bootstrap = require('../bower_components/bootstrap4/dist/js/bootstrap.bundle.js');
 var bootstrapStylev = require('../bower_components/bootstrap4/dist/css/bootstrap.css');
 var recipes = require('./recipes/recipescontroller');
 var requests = require('./requests/requests');
@@ -9,25 +9,15 @@ var requests = require('./signup/signupcontroller');
 var signupTemplate = require('./signup/signup.html');
 var signinTemplate = require('./signup/signin.html');
 var recipeTemplate = require('./recipes/recipes.html');
-var auth0 = require('../bower_components/auth0.js/build/auth0');
-var angularauth = require('../bower_components/angular-auth0/src/index');
-var auth0service = require('./Auth/authService');
-var navbarDirective = require('./Navbar/navbar.directive');
-var navbarTemplate = require('./Navbar/navbar.html');
-var callback = require('./Callback/callback.controller');
-var callbackTemplate = require('./Callback/callback.html');
 var style = require('./styles/style.css');
 
 const app = angular.module('myApp', ['ngRoute',
     'myApp.recipes',
     'myApp.requests',
-    'myApp.signup',
-    'auth0.auth0',
-    'myApp.auth0Service',
-    'myApp.auth0Directive',
-    'myApp.callBackController'
-]);
-app.config(['$routeProvider','angularAuth0Provider','$locationProvider', function($routeProvider,angularAuth0Provider,$locationProvider) {
+    'myApp.signup'
+
+])
+.config(['$routeProvider', function($routeProvider,$httpProvider) {
     $routeProvider.
     when('/', {
         template: signinTemplate,

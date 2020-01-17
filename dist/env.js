34e235f82d605a3e104944608b519c604d5ebbb4
eric periou <eperiou@yahoo.com>
2020-01-17 16:05:36 -0600

    (ignore) change ignore

---
+++ dist/env.js
@@ -0,0 +1,13 @@
+(function(window) {
+    window.__env = window.__env || {};
+
+    // API url
+    // For demo purposes we fetch from local file in this plunk
+    // In your application this can be a url like https://api.github.com
+    window.__env.apiUrl = 'http://localhost:3000';
+    window.__env.hostURL = 'http://localhost:3000';
+
+    // Whether or not to enable debug mode
+    // Setting this to false will disable console output
+    window.__env.enableDebug = true;
+}(this));
